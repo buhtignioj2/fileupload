c02bc7f496c19c7f589e57f0e035a305cff7ba8d
buhtignioj2 <rltgjq@gmail.com>
2018-03-26 15:39:15 +0900

    끝!

--- src/com/example/fileupload/controller/FileUploadController.java
+++ src/com/example/fileupload/controller/FileUploadController.java
@@ -7,16 +7,24 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 
+import com.example.fileupload.service.FileuploadService;
+
 @Controller
 public class FileUploadController {
-	
-	@RequestMapping( "/form" )
-	public String form() {
-		return "form";
-	}
-	
-	@RequestMapping( "/upload" )
-	public String upload() {
-		return "result";
-	}
+
+    @Autowired
+    FileuploadService fileuploadService;
+
+    @RequestMapping("/form")
+    public String form() {
+	return "form";
+    }
+
+    @RequestMapping("/upload")
+    public String upload( Model model, @RequestParam("email") String eamil, @RequestParam("file1") MultipartFile multipartFile) {
+
+	String url = fileuploadService.restore( multipartFile );
+	model.addAttribute( "url", url );
+	return "result";
+    }
 }
